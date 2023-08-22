4d17b90377af446e0f593fd154ccb761a00b1c31
ott666 <ottaviocavarzani@gmail.com>
2023-08-22 12:13:45 -0300

    correcao ctas 3

--- components/CtaServico/CtaServico.tsx
+++ components/CtaServico/CtaServico.tsx
@@ -26,7 +26,7 @@ export default function CtaServico(props:any){
                
                 
                 `}> 
-                    <div className='absolute w-[700px] h-[700px] right-14'>
+                    <div className='absolute invisible 2xl:visible 2xl:w-[500px] 2xl:h-[500px] 3xl:w-[700px] 3xl:h-[700px] right-14'>
                         <Image src={'/Logo-HH-PNG.png'} alt='logo' style={{objectFit:'cover', zIndex:2}} fill={true}/>
                     </div>
                     <div id='wrapHeroContent' 
@@ -55,7 +55,7 @@ export default function CtaServico(props:any){
 
                         <h1 id='heroTitle' 
                         className={`
-
+                            text-start
                        
                             text-[22px]
                             sm:text-[44px]
@@ -63,7 +63,7 @@ export default function CtaServico(props:any){
                             3xl:text-[72px]
 
                         `}> 
-                               {props.ctaTitle}
+                               {props.servico.ctaTitle}
                         </h1>
 
                         <p id='paragraphHero' 
@@ -78,7 +78,7 @@ export default function CtaServico(props:any){
                                 xl:text-[24px]
                                 3xl:text-[28px] 
                         `}>
-                            {props.servico.ctaText}
+                            {props.servico.ctaTexto}
                         </p>
 
                         <div 
@@ -173,7 +173,7 @@ export default function CtaServico(props:any){
                     objectFit:'cover',
                     zIndex:'-2'
                    }}
-                   src={`/services/${props.pastaFotos}}/img2.png`} fill={true} alt='bg'/>
+                   src={`/services/${props.servico.pastaFotos}/img2.png`} fill={true} alt='bg'/>
                 </div>
             </div>
         
